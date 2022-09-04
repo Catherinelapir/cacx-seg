2ed352b2dceab05e4c4b4d67931484a8f361df24
ben-wycliff <ben12wycliff@gmail.com>
2022-09-04 23:46:15 +0300

    Implements image loading

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
-from utils import load_model
+from flask import Flask, render_template, request, redirect, url_for, session
+from matplotlib import image
+from utils import load_model, predict
 
 app = Flask(__name__)
 
 # load model
 model = load_model("./models/my_model.h5")
 
+# pred = predict("./test.png", model)
+# image.imsave("pred.png", pred, cmap="gray")
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", image=False)
+
+
+@app.route("/", methods=["POST"])
+def predict_image():
+    image_upload = request.files["image"]
+    if image_upload != "":
+        image_upload.save("./static/test.png")
+    return render_template("index.html", image=True, image_url="/static/test.png")
